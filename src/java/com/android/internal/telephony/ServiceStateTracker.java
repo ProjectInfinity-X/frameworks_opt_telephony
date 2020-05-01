f04e1951dbc0519c75d82d7bdfb6560e931c32ca
Pranav Vashi <neobuddy89@gmail.com>
2020-05-01 18:14:00 +0530

    Telephony: Fix crash while updating Operator Name
    * Log: https://del.dog/wycylethek
    
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- src/java/com/android/internal/telephony/ServiceStateTracker.java
+++ src/java/com/android/internal/telephony/ServiceStateTracker.java
@@ -4915,7 +4915,7 @@ public class ServiceStateTracker extends Handler {
             return;
         }
         for (CellInfo cellInfo : cellInfos) {
-            if (cellInfo.isRegistered()) {
+            if (cellInfo != null && cellInfo.isRegistered()) {
                 updateOperatorNameForCellIdentity(cellInfo.getCellIdentity());
             }
         }
